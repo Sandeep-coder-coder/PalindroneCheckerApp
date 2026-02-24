997a5125a64a99166642b6c05e0eadee9b8e8618
Sandeep-coder-coder <ss6417@srmist.edu.in>
2026-02-24 15:15:55 +0530

    UC2: Print a Hardcoded Palindrome Result

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,20 @@
+import java.util.*;
 public class PalindromeCheckerApp {
     public static void main (String[] args){
-        System.out.println("Welcome to Palindrome Checker App Management System");
+       Scanner scnr=new Scanner(System.in);
+       System.out.print("Input text:");
+       String str=scnr.nextLine();
+       int a=0;
+       for(int i=0;i<str.length()/2;i++)
+        {
+            if(str.charAt(i)==str.charAt(str.length()-i-1))
+                a++;
+        }
+        System.out.print("\nIs it Palindrome? :");
+       if(a>=str.length()/2)
+           System.out.println(" true");
+       else
+           System.out.println(" false");
+
     }
 }
